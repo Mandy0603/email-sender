236d7289b4e3067181bdfbb7e183aeb25ddf715f
Mandy Ma <yandim0317@gmail.com>
2019-08-03 16:46:59 +1000

    Added surveys model

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const path = require("path");
 
 const keys = require("./config/keys");
 require("./models/User");
+require("./models/Survey");
 require("./services/passport");
 
 mongoose.connect(keys.mongoURI, { useNewUrlParser: true });
@@ -23,6 +24,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 require("./routes/authRoutes")(app);
 require("./routes/billingRoutes")(app);
+require("./routes/surveyRoutes")(app);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
